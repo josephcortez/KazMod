db7f42a211b1826e4d4c5ab27e7f6de45088b55d
Joseph Cortez <joseph.cortez@ufl.edu>
2023-08-13 16:41:32 -0400

    initial commit

---
+++ src/main/java/de/kazzius/kazmod/KazMod.java
@@ -0,0 +1,13 @@
+package de.kazzius.kazmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KazMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/kazzius/kazmod/client/KazModClient.java
@@ -0,0 +1,13 @@
+package de.kazzius.kazmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KazModClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
